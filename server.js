c7341111ad93384433368ac929689f72574d6c1a
Aaron-Fuller <aaron.fuller@protonmail.com>
2019-07-25 20:29:58 -0400

    update

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
-const express = require('express');
+const express = require('espress');
+
+const port = 5000;
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send('Hello World');
-});
+server.use(express.json());
 
 server.get('/hobbits', (req, res) => {
-  const hobbits = [
-    {
-      id: 1,
-      name: 'Samwise Gamgee',
-    },
-    {
-      id: 2,
-      name: 'Frodo Baggins',
-    },
-  ];
-
-  res.status(200).json(hobbits);
+    res.send('Welcome to hobbiton')
 });
 
-server.listen(8000, () => console.log('API running on port 8000'));
+server.post('/hobbits', (req, res) => {
+    res.status(201).json({ usr: '/hobbits', opperation: 'POST'});
+});
+
+server.put('/hobbits', (req, res) => {
+    res.status(200).json({ url: '/hobbits', operation: 'PUT' });
+  });
+
+server.delete('/hobbits', (req, res) => {
+    res.status(204);
+  });
+
+server.listen(port, () => {
+    console.log(`server listening on port ${port}`)
+});
+
